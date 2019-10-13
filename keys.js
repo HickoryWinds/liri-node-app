d2f0e3f3989f116030de50de74578e478cba4633
Grievous Angel <ronfud2000@gmail.com>
2019-10-13 17:05:14 -0400

    all api inquiries and responses functional

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
-console.log("this is loaded");
+console.log("\nthis is loaded");
+
+// console.log(process.env);
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+// console.log("keys ");
+// console.log(exports.spotify);
--- liri.js
+++ liri.js
@@ -1,7 +1,209 @@
-requestAnimationFrame("dotenv").config();
+require("dotenv").config();
+
+var moment = require("moment");
+
+var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 
-var spotify = new spotify(keys.spotify);
+// console.log("liri");
+// console.log(keys.spotify);
+
+var spotify = new Spotify(keys.spotify);
+// console.log(spotify);
+
+// requestAnimationFrame("dotenv").config();
 
 // commands: concert-this, spotify-this-song, movie-this, do-what-it-says
+
+// Include the axios npm package
+var axios = require("axios");
+// include file handling to read random.txt
+var fs = require("fs");
+// Get the name of the search option and the name to search for
+var nodeArg = process.argv;
+// Search option
+var searchType = nodeArg[2];
+// Title of search for
+var name = [];
+// var searchName = "";
+for (var i = 3; i < nodeArg.length; i++) {
+    name.push(nodeArg[i]);
+    // console.log(name);
+}
+// var searchName = name.join(" ");
+// movieName1 = process.argv[3];
+// movieName2 = process.argv[4];
+// ...
+// console.log(movieName1 + " " + movieName2);
+// console.log(searchName);
+
+
+
+if (searchType == "movie-this") {
+    movieThis(name)
+}
+function movieThis(name) {
+    // start movie query
+    var searchName = name.join(" ");
+    if (searchName === "") {
+        searchName = "Mr. Nobody";
+    } 
+    console.log("search " + searchName);
+    // Then run an axios request to movieThis API
+    var queryUrl_OMDB = "http://www.omdbapi.com/?t=" + searchName + "&y=short&apikey=trilogy";
+    // Debug code
+    // console.log(queryUrl_OMDB);
+    axios.get(queryUrl_OMDB).then(
+        function(response) {
+            console.log("\nMovie Title: " + response.data.Title);
+            console.log("Year Released: "  + response.data.Year);
+            console.log("IMBD Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Made in: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Cast: " + response.data.Actors);
+            // console.log(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+            if (error.response) {
+                // }
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+    };
+    //end movie query
+    
+
+
+
+if (searchType == "concert-this") {
+ concertThis(name);
+}
+function concertThis(name) {
+    // start bands in town query
+    var searchName = name.join("+");
+    // Then run an axios request to Bands in Town API
+    // var queryUrl_BIT = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp";
+    var queryUrl_BIT = "https://rest.bandsintown.com/artists/" + searchName + "/events?app_id=codingbootcamp";
+    // Debug code
+    // console.log(queryUrl);
+    axios.get(queryUrl_BIT).then(
+        function(response) {
+            var count = Object.keys(response.data).length;
+            // console.log(typeof(count));
+            // console.log(count);
+            // console.log("----------------");
+            if (count === 0 || (response.data[0].venue == undefined)) {
+                console.log("\nNo concert listings found.")
+            } else {
+                for (var i = 0; i < count; i++) {
+                    console.log("\nConcert Listing " + (i + 1));
+                    // console.log(JSON.stringify(response.data[i]));
+                    // console.log(response.data[i]);
+                    console.log(response.data[i].venue.name);
+                    console.log(response.data[i].venue.city + " " + response.data[i].venue.region + " " + response.data[i].venue.country);
+                    var dateTime = response.data[i].datetime;
+                    console.log(moment(dateTime).format("MM/DD/YYYY"));
+                }
+            }
+        })
+        .catch(function(error) {
+            if (error.response) {
+                // }
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+        // end bands in town query
+    }
+
+
+if (searchType == "spotify-this-song") {
+    spotThis(name);
+}
+function spotThis(name) {
+    // console.log("s " + name);
+// start spotify query
+var searchName = name.join(" ");
+// spotify.search({ type: 'track', query: 'All the Small Things', limit: '5' }, function(err, data) {
+spotify.search({ type: 'track', query: searchName, limit: '5' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    for (var j = 0; j < 2; j++) {
+        
+        //   console.log(data.tracks.items[0].artists); 
+        //   console.log(data.tracks.items[0].name); 
+        //   console.log(data.tracks.items[0].album.name); 
+        //   console.log(data.tracks.items[0].album.artists[0].name); 
+        //   console.log(data.tracks.items[0].preview_url); 
+        console.log(data.tracks.items[j].name); 
+        console.log(data.tracks.items[j].album.name); 
+        console.log(data.tracks.items[j].album.artists[0].name); 
+        console.log(data.tracks.items[j].preview_url); 
+        // console.log(JSON.stringify(data.tracks.items[j].album)); 
+        // console.log(data.tracks); 
+        
+    }
+    
+});
+// end spotify query
+}
+   
+if (searchType == "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+          }
+          // We will then print the contents of data
+        //   console.log(data);
+          // Then split it by commas (to make it more readable)
+          var dataArr = data.split(",");
+          // We will then re-display the content as an array for later use.
+          var operation = dataArr[0];
+          var lookFor = dataArr[1].split(" ");
+        //   console.log(operation + " " + lookFor);
+        //   var doThis = "node " + "liri.js " + operation + " " + lookFor;
+        //   console.log(doThis);
+          if (operation == "spotify-this-song") {
+              spotThis(lookFor);
+          }
+          if (operation == "concert-this") {
+              concertThis(lookFor);
+          }
+          if (operation == "movie-this") {
+              movieThis(lookFor);
+          }
+    });
+}
